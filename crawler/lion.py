efae359cf5eea87fe9924e645be609878041147c
sarah <sarahcheng0122@gmail.com>
2018-10-07 15:26:03 +0800

    lion debug

--- crawler/lion.py
+++ crawler/lion.py
@@ -9,7 +9,7 @@ from crawler.setting import Setting
 class Lion(object):
     def __init__(self, tag_code):
         now = datetime.datetime.now().strftime("%Y-%m-%d")
-        self.url = "https://travel.liontravel.com/search?Country=TW&WebCode=B2C&TravelType=1&Page=1&PageSize=1000&DepartureID=&GoDateStart="+ now + "&GoDateEnd=2018-10-05&IsEnsureGroup=false&ArriveID=" + tag_code
+        self.url = "https://travel.liontravel.com/search?Country=TW&WebCode=B2C&TravelType=1&Page=1&PageSize=1000&DepartureID=&GoDateStart="+ now + "&GoDateEnd=2019-03-31&IsEnsureGroup=false&ArriveID=" + tag_code
         print(self.url)
         self.code = tag_code
         self.count = 0
@@ -30,7 +30,11 @@ class Lion(object):
             date = item.select("[class='dates_info']")[0].find_all('a')
             for i in range(len(date)):
                 tmp_date = date[i].text[:5].split('/') #ex:12/21
-                convertDate = datetime.date(2018,int(tmp_date[0]),int(tmp_date[1]))
+                if(int(tmp_date[0])<=3):
+                    tmp_year = 2019
+                else:
+                    tmp_year= 2018
+                convertDate = datetime.date(tmp_year,int(tmp_date[0]),int(tmp_date[1]))
                 self.itinerary['departure_date'].append(convertDate)
                 tmp_link = ("https://travel.liontravel.com"+date[i].get('href'))
                 self.itinerary['link'].append(tmp_link)
@@ -42,48 +46,49 @@ class Lion(object):
             self.itinerary['price'] = tmp_price
             self.itinerary['title'] = tmp_title[0]
             self.getDetail(tmp_link)
-
+            print(self.itinerary['detail'])
             items.append(self.itinerary)
             self.resetItinerary() #reset itinerary to empty
-            print('Crawling and deposit {} data from {}'.format(self.count, self.code))
+            print('Crawling {} data from {}'.format(self.count, self.code))
         while items:
             lion = Deposit(self.code, items.pop())
             lion.run()
+            print('Saving data from {}'.format(self.code))
             
 
     def getDetail(self, link):
-        res = requests.get(link)
-        soup = BeautifulSoup(res.text, 'lxml')
-        try:
-            detail = soup.select("[class='clp-subtitle font-16']")
-            day_count = 0
-            detail_dic = {}
-            for item in detail:
-                day_count += 1
-                detail_dic[("DAY " + str(day_count))] = item.text
-            self.itinerary['detail'] = detail_dic
-        except:
-            no_detail = {"notice":"此項目無行程內容"}
-            self.itinerary['detail'] = no_detail
-
-        # setting = Setting()
-        # browser = setting.settingDriver()
-        # browser.get(self.url)
+        # res2 = requests.get(link)
+        # soup = BeautifulSoup(res2.text, 'lxml')
         # try:
-        #     browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-            
-        #     browser.find_element_by_xpath("//a[contains(text(),'行程内容')]").click()
-        #     detail = (browser.find_elements_by_xpath("//div[@class='clp-header md-top-n']/p[2]"))
+        #     detail = soup.select("[class='clp-subtitle font-16']")
         #     day_count = 0
         #     detail_dic = {}
-        #     for item in detail:
+        #     for item in detail:   
         #         day_count += 1
         #         detail_dic[("DAY " + str(day_count))] = item.text
         #     self.itinerary['detail'] = detail_dic
         # except:
         #     no_detail = {"notice":"此項目無行程內容"}
         #     self.itinerary['detail'] = no_detail
-        # browser.close()
+
+        setting = Setting()
+        browser = setting.settingDriver()
+        browser.get(link)
+        try:
+            browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+            
+            browser.find_element_by_xpath("//a[contains(text(),'行程内容')]").click()
+            detail = (browser.find_elements_by_xpath("//div[@class='clp-header md-top-n']/p[2]"))
+            day_count = 0
+            detail_dic = {}
+            for item in detail:
+                day_count += 1
+                detail_dic[("DAY " + str(day_count))] = item.text.replace('\n', '')
+            self.itinerary['detail'] = detail_dic
+        except:
+            no_detail = {"notice":"此項目無行程內容"}
+            self.itinerary['detail'] = no_detail
+        browser.close()
 
 
 
